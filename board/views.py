e90dbd8395e4743d625e269c0174d7bd0bf9b291
Hepheir <hepheir@gmail.com>
2023-05-28 21:26:00 +0900

    feat(board): create views.index()

--- board/views.py
+++ board/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+from .models import Post
+from .utils import Post_to_JSON
 
 # Create your views here.
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        'posts': list(map(Post_to_JSON, Post.objects.all())),
+    }
+    print(context)
+    return render(request, "board.html", context)
