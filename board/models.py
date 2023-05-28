6542400de5dc68cffd965ce999ab4a362eec9c9a
Hepheir <hepheir@gmail.com>
2023-05-28 20:34:16 +0900

    feat(board): create Reply model

--- board/models.py
+++ board/models.py
@@ -17,3 +17,9 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, null=False, on_delete=models.CASCADE)
     author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
+
+
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment, null=False, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
