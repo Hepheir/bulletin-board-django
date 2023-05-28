ffbdb71c064949f6eb630f1334bd556a4390daec
Hepheir <hepheir@gmail.com>
2023-05-28 20:30:32 +0900

    feat(board): create Post model

--- board/models.py
+++ board/models.py
@@ -6,3 +6,8 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(null=False, max_length=16)
     password = models.CharField(null=False, max_length=16)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
