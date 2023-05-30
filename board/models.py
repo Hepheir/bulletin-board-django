908a07462ba8ac19811c85fce23129cffd5bc188
Hepheir <hepheir@gmail.com>
2023-05-31 01:55:39 +0900

    fix(board)!: disallow duplicate username

--- board/models.py
+++ board/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(null=False, max_length=16)
+    username = models.CharField(null=False, max_length=16, unique=True)
     password = models.CharField(null=False, max_length=16)
 
 
