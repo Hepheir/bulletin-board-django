78874a5aaed410c31c07aef664186851086e11de
Hepheir <hepheir@gmail.com>
2023-05-31 02:23:24 +0900

    fix(board): disallow null content in models

--- board/models.py
+++ board/models.py
@@ -12,19 +12,19 @@ class User(models.Model):
 class Post(models.Model):
     author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
     title = models.CharField(null=False, max_length=80)
-    content = models.CharField(max_length=1000)
+    content = models.CharField(null=False, max_length=1000)
     created_at = models.DateTimeField(null=False, auto_now_add=True)
 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, null=False, on_delete=models.CASCADE)
     author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
-    content = models.CharField(max_length=200)
+    content = models.CharField(null=False, max_length=200)
     created_at = models.DateTimeField(null=False, auto_now_add=True)
 
 
 class Reply(models.Model):
     comment = models.ForeignKey(Comment, null=False, on_delete=models.CASCADE)
     author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
-    content = models.CharField(max_length=200)
+    content = models.CharField(null=False, max_length=200)
     created_at = models.DateTimeField(null=False, auto_now_add=True)
