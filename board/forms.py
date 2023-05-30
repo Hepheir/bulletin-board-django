3c71b77ded722ecc044d36f2714f6418f6dc6417
Hepheir <hepheir@gmail.com>
2023-05-31 05:12:06 +0900

    feat(board): create PostForm

--- board/forms.py
+++ board/forms.py
@@ -7,3 +7,9 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'created_by']
