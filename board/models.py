8d6556df794d378ea18296b91ad76648d7f733cb
Hepheir <hepheir@gmail.com>
2023-05-31 02:05:49 +0900

    feat(board)!: add column "created_at" for all models

--- board/models.py
+++ board/models.py
@@ -6,20 +6,24 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(null=False, max_length=16, unique=True)
     password = models.CharField(null=False, max_length=16)
+    created_at = models.DateTimeField(null=False, auto_now_add=True)
 
 
 class Post(models.Model):
     author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(null=False, auto_now_add=True)
 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, null=False, on_delete=models.CASCADE)
     author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(null=False, auto_now_add=True)
 
 
 class Reply(models.Model):
     comment = models.ForeignKey(Comment, null=False, on_delete=models.CASCADE)
     author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(null=False, auto_now_add=True)
