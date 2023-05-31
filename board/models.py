927ac7e774404db51ecea756952cac8dc97ff78f
Hepheir <hepheir@gmail.com>
2023-05-31 12:29:29 +0900

    feat(board): add reverse lookup of model Post, Comment

--- board/models.py
+++ board/models.py
@@ -18,12 +18,20 @@ class Post(models.Model):
     created_at = models.DateTimeField(null=False, auto_now_add=True)
     created_by = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
 
+    @property
+    def comments(self):
+        return Comment.objects.filter(post=self)
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, null=False, on_delete=models.CASCADE)
     content = models.CharField(null=False, max_length=200)
     created_at = models.DateTimeField(null=False, auto_now_add=True)
     created_by = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+
+    @property
+    def replies(self):
+        return Reply.objects.filter(comment=self)
 
 
 class Reply(models.Model):
