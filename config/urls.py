cf13f8f9014d22427b98be889accda8cd0c345e7
Hepheir <hepheir@gmail.com>
2023-05-31 12:53:46 +0900

    feat(board): assign CommentUpdateView to "board/comment/<int:pk>/update/"

--- config/urls.py
+++ config/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path("board/post/<int:pk>/", views.PostDetailView.as_view()),
     path("board/post/<int:pk>/update/", views.PostUpdateView.as_view()),
     path("board/comment/create/", views.CommentCreateView.as_view()),
+    path("board/comment/<int:pk>/update/", views.CommentUpdateView.as_view()),
     path("board/user/create/", views.UserCreateView.as_view()),
     path("board/user/list/", views.UserListView.as_view()),
 ]
