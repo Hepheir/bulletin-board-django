9d1e3e5f75f8b8eef0852b1f16044bebee46268a
Hepheir <hepheir@gmail.com>
2023-06-04 15:03:13 +0900

    fix(board/models): add custom UserManager to fix Attribute Error at "/admin/login/"
    ```
    AttributeError at /admin/login/
    'Manager' object has no attribute 'get_by_natural_key'
    Request Method:	POST
    Request URL:	http://127.0.0.1:8000/admin/login/?next=/admin/
    Django Version:	4.2.1
    Exception Type:	AttributeError
    Exception Value:
    'Manager' object has no attribute 'get_by_natural_key'
    Raised during:	django.contrib.admin.sites.login
    Python Version:	3.10.0
    ```

--- board/models.py
+++ board/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def get_by_natural_key(self, username: str | None):
+        return super().get_by_natural_key(username)
 
 
 class User(AbstractBaseUser):
     username = models.CharField(null=False, max_length=16, unique=True)
     created_at = models.DateTimeField(null=False, auto_now_add=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
