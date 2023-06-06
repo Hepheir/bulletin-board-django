0c0e7cb59dcf5d5e7fbc74df78a2cc9ff3f6a5d9
Hepheir <hepheir@gmail.com>
2023-06-07 03:33:11 +0900

    feat(board/views): CRUD 후 리디렉션 주소를 reverse() 함수를 이용하여 가져오도록 수정

--- board/views.py
+++ board/views.py
@@ -1,5 +1,6 @@
 from django.forms.models import BaseModelForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.http import HttpRequest, HttpResponse
 from django.views.generic.base import View
 from django.views.generic.detail import DetailView
@@ -26,7 +27,7 @@ class BoardView(View):
 class UserCreateView(CreateView):
     model = User
     form_class = UserCreateForm
-    success_url = '/'
+    success_url = reverse_lazy('home')
 
     def form_valid(self, form: BaseModelForm) -> HttpResponse:
         return super().form_valid(form)
@@ -40,7 +41,7 @@ class UserListView(ListView):
 class PostCreateView(CreateView):
     model = Post
     form_class = PostCreateForm
-    success_url = '/'
+    success_url = reverse_lazy('home')
 
 
 class PostListView(ListView):
@@ -55,43 +56,43 @@ class PostDetailView(DetailView):
 class PostUpdateView(UpdateView):
     model = Post
     form_class = PostUpdateForm
-    success_url = '/'
+    success_url = reverse_lazy('home')
 
 
 class PostDeleteView(DeleteView):
     model = Post
-    success_url = '/'
+    success_url = reverse_lazy('home')
 
 
 class CommentCreateView(CreateView):
     model = Comment
     form_class = CommentCreateForm
-    success_url = '/'
+    success_url = reverse_lazy('home')
 
 
 class CommentUpdateView(UpdateView):
     model = Comment
     form_class = CommentUpdateForm
-    success_url = '/'
+    success_url = reverse_lazy('home')
 
 
 class CommentDeleteView(DeleteView):
     model = Comment
-    success_url = '/'
+    success_url = reverse_lazy('home')
 
 
 class ReplyCreateView(CreateView):
     model = Reply
     form_class = ReplyCreateForm
-    success_url = '/'
+    success_url = reverse_lazy('home')
 
 
 class ReplyUpdateView(UpdateView):
     model = Reply
     form_class = ReplyUpdateForm
-    success_url = '/'
+    success_url = reverse_lazy('home')
 
 
 class ReplyDeleteView(DeleteView):
     model = Reply
-    success_url = '/'
+    success_url = reverse_lazy('home')
