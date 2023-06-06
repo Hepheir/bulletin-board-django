579906bc4eb11b7fbb5d93e565b574c18273abc5
Hepheir <hepheir@gmail.com>
2023-06-06 23:06:54 +0900

    refactor: Path 클래스의 연산자 오버라이딩 사용

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_file = BASE_DIR.joinpath('secret.json')
+secret_file = BASE_DIR / 'secret.json'
 secrets: Dict[str, Any] = {}
 
 with open(secret_file, 'r') as f:
